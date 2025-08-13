90f715b5683e69bb712f236630799cb0bcf8f4c9
DonChenn <donovaac@uci.edu>
2025-08-13 16:41:43 -0700

    testing iframe

--- src/js/background_music.js
+++ src/js/background_music.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const water_background = document.getElementById('background-music');
     const saved_time = sessionStorage.getItem('music_time');
 
-    water_background.play();
-
     if (water_background) {
         if (saved_time) {
             water_background.currentTime = parseFloat(saved_time);
